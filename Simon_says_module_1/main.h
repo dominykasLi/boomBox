cbcd1e2376cb320bc602074a628514d270ef410d
Dominykas Litvaitis <dominykas.litvaitis@gmail.com>
2020-05-06 15:39:17 +0300

    Initial commit of Simon says module

---
+++ Simon_says_module_1/main.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.h
+ * Author: Barnie
+ *
+ * Created on July 7, 2019, 7:44 PM
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MAIN_H */
+
